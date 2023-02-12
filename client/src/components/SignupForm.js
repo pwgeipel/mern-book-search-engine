ef2b4030fd210680a8e2532558ae16b0f13d3fd9
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-02-11 21:30:56 -0600

    loginForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -32,7 +32,7 @@ const SignupForm = () => {
     }
 
     try {
-      const {data} = await addUser({variables: { ...userFormData }
+      const { data } = await addUser({variables: { ...userFormData }
       })
       console.log(data)
       Auth.login(data.addUser.token)
