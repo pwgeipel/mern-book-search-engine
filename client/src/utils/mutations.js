c114d2644d274dd4ab70232ac8528c49e70b1760
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-02-19 15:28:32 -0600

    server.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,57 +1,37 @@
 import { gql } from '@apollo/client'
 
 export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
+    mutation Login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
         token
         user {
-            email
+            _id
             username
+            email
         }
         }
     }
     `;
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
+    mutation AddUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
         token
         user {
             _id
-            email
             username
+            email
         }
         }
     }
 `;
 
 export const SAVE_BOOK = gql`
-mutation SaveBook($bookData: BookInput!) {
-    saveBook(bookData: $bookData) {
-      _id
-      username
-      email
-      savedBooks {
-        authors
-        description
-        bookId
-        image
-        link
-        title
-      }
-      bookCount
-    }
-  }
-
-`;
-
-export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: ID!) {
-        removeBook(bookId: $bookId) {
-        email
-        username
+    mutation SaveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
         _id
-        bookCount
+        username
+        email
         savedBooks {
             authors
             description
@@ -60,6 +40,27 @@ export const REMOVE_BOOK = gql`
             link
             title
         }
+        bookCount
+        }
+    }
+
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation RemoveBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+        bookCount
         }
     }
 
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
+const path = require ('path');
+const apiRoutes = require('.api')
+// const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'))
+});
 
 module.exports = router;
