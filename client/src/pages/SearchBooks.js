c82b5fe413dba0d238828e064b253a729bcb89d3
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-02-19 12:27:47 -0600

    changed import SearchBooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-import { useMutation } from '../utils/mutations'
+import { useMutation } from '@apollo/client'
 import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
@@ -14,6 +14,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -68,7 +70,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const { data } = await saveBookIds({
+      const { data } = await saveBook({
         variables: { bookData: { ...bookToSave
         }}
       })
